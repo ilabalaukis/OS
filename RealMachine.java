ea2d5eaea483a0d845af34b3a1b5642dfdc1bcc2
Domas <Domcikas@gmail.com>
2018-03-10 21:09:30 +0200

    Komentaras

--- RealMachine.java
+++ RealMachine.java
@@ -1,4 +1,23 @@
 public class RealMachine
 {
-	
+	private int[] userMemory = int[1500];
+	//Processor
+	//Data Registers
+	private int DRA = 0000;
+	private int DRB = 0000;
+	//Interrupt Registers
+	private int TI = 0000;
+	private int PI = 0000;
+	private int IOI = 0000;
+	private int SI = 0000;
+	//Segment Registers
+	private int DS = 0000;
+	private int CS = 0700;
+	private int SS = 1400;
+	//Other Registers
+	private int IC = 0000;
+	private int SP = 0000;
+	private int PTR = 0000;
+	private boolean MODE = 0000;
+	private int SF = 0000;
 }
