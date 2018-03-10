398e75c29362e99f683a7454ed76ba0296d3d27f
Ignas Labalaukis <ignas.labalaukis@mif.stud.vu.lt>
2018-03-10 21:35:52 +0200

    seteriai ir geteriai.

--- RealMachine.java
+++ RealMachine.java
@@ -22,28 +22,125 @@ public class RealMachine
 	private int SF = 0000;
 	
 	//getters and setters
-	private int getDRA()
+	public int getDRA()
 	{
 		return this.DRA;
 	}
-	private void setDRA(int DRA)
+	public void setDRA(int DRA)
 	{
 		this.DRA = DRA;
 	}
-	private int getDRB()
+	public int getDRB()
 	{
 		return this.DRB;
 	}
-	private void setDRB(int DRB)
+	public void setDRB(int DRB)
 	{
 		this.DRB = DRB;
 	}
-	private int getTI()
+	public int getTI()
 	{
 		return this.TI;
 	}
-	private void setTI(int TI)
+	public void setTI(int TI)
 	{
 		this.TI = TI;
 	}
+	public int getPI()
+	{
+		return this.PI;
+	}
+	public void setPI(int PI)
+	{
+		this.PI = PI;
+	}
+	public int getIOI()
+	{
+		return this.IOI;
+	}
+	public void setIOI(int IOI)
+	{
+		this.IOI = IOI;
+	}
+	public int getSI()
+	{
+		return this.SI;
+	}
+	public void setSI(int setSI)
+	{
+		this.SI = SI;
+	}
+	public int getDS()
+	{
+		return this.DS;
+	}
+	public void setDS(int DS)
+	{
+		this.DS = DS;
+	}
+	public int getCS()
+	{
+		return this.CS;
+	}
+	public void setCS(int CS)
+	{
+		this.CS = CS;
+	}
+	public int getCS()
+	{
+		return this.CS;
+	}
+	public void setCS(int CS)
+	{
+		this.CS = CS;
+	}
+	public int getSS()
+	{
+		return this.SS;
+	}
+	public void getSS(int SS)
+	{
+		this.SS = SS;
+	}
+	public int getIC()
+	{
+		return this.IC;
+	}
+	public void setIC(int IC)
+	{
+		this.IC = IC;
+	}
+	public int getSP()
+	{
+		return this.SP;
+	}
+	public void setSP(int SP)
+	{
+		this.SP = SP;
+	}
+	public int getPTR()
+	{
+		return this.PTR;
+	}
+	public void setPTR(int PTR)
+	{
+		this.PTR = PTR;
+	}
+	public int getMODE()
+	{
+		return this.MODE;
+	}
+	public void setMODE(int MODE)
+	{
+		this.MODE = MODE;
+	}
+	public int getSF()
+	{
+		return this.SF;
+	}
+	public void setSF(int SF)
+	{
+		this.SF = SF;
+	}
 }
+
