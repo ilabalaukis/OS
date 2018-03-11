f050165030516432426555cc9ac4441bc6cd1ec7
Domas <Domcikas@gmail.com>
2018-03-11 17:47:45 +0200

    Taisau Igno darbo broką.

--- VirtualMachine.java
+++ VirtualMachine.java
@@ -3,6 +3,7 @@ public class VirtualMachine extends Machine
 	
 	public static RealMachine realMachine;
 	private int[] virtualMemory = new int[15];
-	
-	
+	public VirtualMachine(RealMachine realMachine){
+		this.realMachine = realMachine;
+	}
 }
