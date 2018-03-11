445d7398422891e50787d32a35e86783c4c1a663
Domas <Domcikas@gmail.com>
2018-03-11 23:45:32 +0200

    Maine darau ciklus

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import java.util.*;
+import java.io.*;
 public class Main
 {
 	public static void main(String[] args)
 	{
+		Scanner input = new Scanner(System.in);
 		RealMachine realMachine = new RealMachine();
-		//VirtualMachine vm = new Virtualm(realMachine)
+		VirtualMachine vm = new VirtualMachine(realMachine);
 		HashMap<String, Integer> command = new HashMap<String, Integer>();
+		ArrayList<VirtualMachine> virtualMachines = new ArrayList<VirtualMachine>();
 
 		command.put("ADD", new Integer(0001));
     	command.put("SUB", new Integer(0002));
@@ -35,9 +38,55 @@ public class Main
 
 		command.put("LUM", new Integer(0071));
 		command.put("LEM", new Integer(0072));
-
-		Set set = command.entrySet();
-		
-		Iterator i = set.iterator();
+		System.out.println("Hello, this is Atlas machine.");
+		showCommands();
+		while(true)
+		{
+			String comm = input.next();
+			if( comm.equals("EXIT") ){
+				System.out.println("Goodbye.");
+				break;
+			}
+			switch(comm){
+				case "NEW":
+					System.out.println("NEW called.");
+					break;
+				case "NEWV":
+					System.out.println("NEWV called.");
+					break;
+				case "START":
+					System.out.println("START called.");
+					if( virtualMachines.isEmpty() )
+					{
+						System.out.println("vm is empty");
+						virtualMachines = executeVirtuals(virtualMachines);
+					}
+					else
+					{
+						System.out.println("vm is not empty");
+						
+					}
+					
+					break;
+				case "HELP":
+					showCommands();
+					break;
+				default:
+					System.out.println("Command not found.");
+			}
+			
+		}
+	}
+	
+	public static void showCommands(){
+		System.out.println("Available Commands:");
+		System.out.println("HELP - this help page.");
+		System.out.println("NEW - creates new program.");
+		System.out.println("NEWV - creates new virtual machine.");
+		System.out.println("START - start programs execution mode.");
+		System.out.println("EXIT - turn off the machine.");
+	}
+	public static ArrayList<VirtualMachine> executeVirtuals(ArrayList<VirtualMachine> virtualMachines){
+		return virtualMachines;
 	}
 }
